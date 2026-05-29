1aa9e4723563ae7dafe1701eebd6d41b0d734464
Joel Jacob <108456404+debugst1ck@users.noreply.github.com>
2026-05-29 17:24:06 +1000

    Relax BatchT TypeVar

--- src/tarp/typed/data.py
+++ src/tarp/typed/data.py
@@ -1,9 +1,7 @@
 from collections.abc import Mapping
 from typing import TypeVar
 
-from torch import Tensor
-
 from tarp.typed.core import KnownT
 
 RowT = TypeVar("RowT", bound=Mapping[str, KnownT])
-BatchT = TypeVar("BatchT", bound=Mapping[str, Tensor])
+BatchT = TypeVar("BatchT")
