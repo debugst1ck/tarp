778329e876aa108f2c3f6b6a798f8b2625d77966
Joel Jacob <108456404+debugst1ck@users.noreply.github.com>
2026-05-29 17:22:51 +1000

    Add ESM1b pretrained tokenizer

---
+++ src/tarp/preprocessing/tokenizers/pretrained/esm1b.py
@@ -0,0 +1,40 @@
+from typing import final, override
+
+from torch import Tensor
+from transformers import AutoTokenizer
+
+from tarp.preprocessing.tokenizers.core import Tokenizer
+
+
+@final
+class ESM1bTokenizer(Tokenizer):
+    def __init__(self, name="facebook/esm1b_t33_650M_UR50S"):
+        self.tokenizer = AutoTokenizer.from_pretrained(name, trust_remote_code=True)
+
+    @override
+    def encode(self, text: str) -> Tensor:
+        return self.tokenizer(text, return_tensors="pt")["input_ids"].squeeze(0)
+
+    @override
+    def decode(self, tokens: Tensor) -> str:
+        return self.tokenizer.decode(tokens.tolist(), skip_special_tokens=True)
+
+    @property
+    @override
+    def vocabulary_size(self) -> int:
+        return self.tokenizer.vocab_size
+
+    @property
+    @override
+    def pad_token_id(self) -> int:
+        return self.tokenizer.pad_token_id
+
+    @property
+    @override
+    def mask_token_id(self) -> int:
+        return self.tokenizer.mask_token_id
+
+    @property
+    @override
+    def cls_token_id(self) -> int:
+        return self.tokenizer.cls_token_id
